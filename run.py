f348590084d95a32421f569af9dc9cfbacee42af
CraftersVerse <craftersverse@gmail.com>
2023-12-28 18:56:29 +0800

    add requirements.txt

--- run.py
+++ run.py
@@ -102,4 +102,4 @@ def predict_objects():
     return jsonify({'predicted_images_data': predicted_images_data}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000,debug=True)
